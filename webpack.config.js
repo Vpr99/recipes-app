b209b711514eac86104f46d36aef10caeff2d07a
Eric Skram <eric@skram.com>
2015-08-27 20:11:50 -0700

    Added development server and CSS to Webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,35 @@
 var path = require('path');
 var HtmlwebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
 
 var ROOT_PATH = path.resolve(__dirname);
 
 module.exports = {
-  entry: path.resolve(ROOT_PATH, 'app/main'),
-  output: {
-    path: path.resolve(ROOT_PATH, 'build'),
-    filename: 'bundle.js'
-  },
-  plugins: [
-    new HtmlwebpackPlugin({
-      title: 'Recipes App'
-    })
-  ]
+    entry: path.resolve(ROOT_PATH, 'app/main'),
+    output: {
+        path: path.resolve(ROOT_PATH, 'build'),
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.css$/,
+                loaders: ['style', 'css'],
+                include: path.resolve(ROOT_PATH, 'app')
+            }
+        ]
+    },
+    devServer: {
+        colors: true,
+        historyApiFallback: true,
+        hot: true,
+        inline: true,
+        progress: true
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlwebpackPlugin({
+            title: 'Recipes App'
+        })
+    ]
 };
