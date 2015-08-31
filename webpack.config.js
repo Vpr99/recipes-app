9f32a8c38d3a5d952301ae7cc21c860e7c889ee4
Eric Skram <eric@skram.com>
2015-08-30 17:35:30 -0700

    Added Babel to enable ES6 syntax. Added Webpack config to resolve extensions automatically in require statements.

--- webpack.config.js
+++ webpack.config.js
@@ -15,21 +15,25 @@ var TARGET = process.env.npm_lifecycle_event;
 var ROOT_PATH = path.resolve(__dirname);
 
 var common = {
-  entry: path.resolve(ROOT_PATH, 'app/main'),
-  output: {
-      path: path.resolve(ROOT_PATH, 'build'),
-      filename: 'bundle.js'
-  },
-  module: {
-      loaders: [
-          {
-              test: /\.css$/,
-              loader: "style-loader!css-loader?minimize!postcss-loader",
-              include: path.resolve(ROOT_PATH, 'app')
-          }
-      ]
-  },
-  postcss: function () {
+    entry: path.resolve(ROOT_PATH, 'app/main.jsx'),
+    output: {
+        path: path.resolve(ROOT_PATH, 'build'),
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.css$/,
+                loader: "style-loader!css-loader?minimize!postcss-loader",
+                include: path.resolve(ROOT_PATH, 'app')
+            }
+        ]
+    },
+    resolve: {
+        /* Resolves extensions to require('file') instead of require('file.js') */
+        extensions: ['', '.js', '.jsx', '.json', '.coffee']
+    },
+    postcss: function () {
       return [
         postcssImport({
               onImport: function (files) {
@@ -42,18 +46,27 @@ var common = {
           postcssExtend(),
           postcssLost()
       ];
-  },
-  plugins: [
-      new webpack.HotModuleReplacementPlugin(),
-      new HtmlwebpackPlugin({
-          title: 'Recipes App'
-      })
-  ]
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlwebpackPlugin({
+            title: 'Recipes App'
+        })
+    ]
 }
 
 if(TARGET === 'start' || !TARGET) {
     module.exports = merge(common, {
         devtool: 'eval-source-map',
+        module: {
+            loaders: [
+                {
+                    test: /\.jsx?$/,
+                    loaders: ['react-hot', 'babel'],
+                    include: path.resolve(ROOT_PATH, 'app')
+                }
+            ]
+        },
         devServer: {
             historyApiFallback: true,
             hot: true,
