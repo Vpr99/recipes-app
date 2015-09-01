bfc2bf47a3b9f72aa5f903b88eefb49b9410c163
Eric Skram <eric@skram.com>
2015-08-31 17:51:32 -0700

    Tidy up boilerplate code and Underscore pluralization config.

--- app/components/Recipe.jsx
+++ app/components/Recipe.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import _ from 'underscore';
-//_.mixin(require('underscore.inflections'));
+_.mixin(require('underscore.inflections'));
 
 export default class Recipe extends React.Component {
     render() {
@@ -20,9 +20,9 @@ export default class Recipe extends React.Component {
                 ingredient.name += "s";
             }
             /* Then pluralize the remaining ingredients. */
-            // else if(ingredient.quantity > 1) {
-            //     ingredient.unit = _.pluralize(ingredient.unit);
-            // }
+            else if(ingredient.quantity > 1) {
+                ingredient.unit = _.pluralize(ingredient.unit);
+            }
 
             /* Check to see if the ingredient has an optional instruction */
             if(ingredient.instruction) {
@@ -49,54 +49,3 @@ export default class Recipe extends React.Component {
         );
     }
 }
-
-// import React from 'react';
-//
-// export default class Recipe extends React.Component {
-//     render() {
-//         /* Store the recipe steps */
-//         var steps = [];
-//         _.each(this.props.data.steps, function(step) {
-//             steps.push(<li>{step}</li>)
-//         })
-//
-        /* Process & store the ingredients */
-        // var ingredients = [];
-        // _.each(this.props.data.ingredients, function(ingredient) {
-        //
-        //     var ingredientInstruction;
-        //
-        //     /* Pluralize ingredients without a unit of measurement */
-        //     if(!ingredient.unit && ingredient.quantity > 1) {
-        //         ingredient.name += "s";
-        //     }
-        //     /* Then pluralize the remaining ingredients. */
-        //     else if(ingredient.quantity > 1) {
-        //         ingredient.unit = _.pluralize(ingredient.unit);
-        //     }
-        //
-        //     /* Check to see if the ingredient has an optional instruction */
-        //     if(ingredient.instruction) {
-        //         ingredientInstruction = (<span className="Recipe-ingredient--instruction">, {ingredient.instruction}</span>)
-        //     }
-        //
-        //     /* Add the ingredient to the array */
-        //     ingredients.push(
-        //         <li>{ingredient.quantity} {ingredient.unit} {ingredient.name}{ingredientInstruction}</li>
-        //     )
-        //
-        // })
-//
-//         return (
-//             <div className="Recipe">
-//                 <h2 className="Recipe-title">{this.props.data.title}</h2>
-//                 <div className="Recipe-meta">
-//                     <p className="Recipe-servings">Servings: {this.props.data.servings}</p>
-//                     <p className="Recipe-time">Time Taken: {this.props.data.time}</p>
-//                 </div>
-//                 <ul className="Recipe-ingredients">{ingredients}</ul>
-//                 <ul className="Recipe-steps">{steps}</ul>
-//             </div>
-//         )
-//     }
-// }
